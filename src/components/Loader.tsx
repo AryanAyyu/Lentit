09adb36da36845cd8851eecb1391abb62757abe1
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-04-25 23:42:48 +0530

    loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <div className="h-8 w-8 border-4 border-gray-600 border-solid rounded-full border-t-transparent animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
